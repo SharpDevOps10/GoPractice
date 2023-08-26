c0bcc54b1569e83b0c4c1670b2fefa8d14d6ccdd
SharpDevOps10 <td090833@gmail.com>
2023-08-26 23:54:13 +0300

    Update `Open` function

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -1,7 +1,10 @@
 package store
 
+import "database/sql"
+
 type Store struct {
 	config *Config
+	db     *sql.DB
 }
 
 func New(config *Config) *Store {
@@ -11,5 +14,16 @@ func New(config *Config) *Store {
 }
 
 func (s *Store) Open() error {
+	db, err := sql.Open("postgres", s.config.DatabaseURL)
+	if err != nil {
+		return err
+	}
+
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	s.db = db
+
 	return nil
 }
