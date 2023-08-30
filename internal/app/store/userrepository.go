7d5e0e57acf20bd629692de985748f4487cbc12d
SharpDevOps10 <td090833@gmail.com>
2023-08-30 16:15:13 +0300

    Add function that creates `user`

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -1,5 +1,24 @@
 package store
 
+import "github.com/SharpDevOps10/GoPractice/internal/app/model"
+
 type UserRepository struct {
 	store *Store
+}
+
+func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+	if err := r.store.db.QueryRow(
+		"INSERT INTO userss (email, encrypted_password) VALUES ($1, $2) RETURNING id",
+		u.Email,
+		u.EncryptedPassword,
+	).Scan(&u.ID); err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	return nil, nil
+
 }
