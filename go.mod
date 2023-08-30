d0bc66c4969b5c40f7c8c8c846514a3a76121c65
SharpDevOps10 <td090833@gmail.com>
2023-08-30 20:56:45 +0300

    Implement func for hashing passwords before storing them in DB

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,7 +1,19 @@
 package model
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID                int
 	Email             string
+	Password          string
 	EncryptedPassword string
+}
+
+func encryptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
