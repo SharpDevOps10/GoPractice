2a49866477b25f0b8a495c9ba3527c15e09283bc
SharpDevOps10 <td090833@gmail.com>
2023-08-30 19:30:55 +0300

    Update `FindByEmail` tests: - user`s already existed in DB

--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -25,4 +25,11 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
+	s.User().Create(&model.User{
+		Email: "bulldog@gmail.com",
+	})
+	u, err := s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+
 }
