0f131199bfeacf80f4e881b3bbebd8fd1df9990e
SharpDevOps10 <td090833@gmail.com>
2023-09-01 22:12:47 +0300

    Update `TestUser_Validate`: add validation cases

--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -7,8 +7,30 @@ import (
 )
 
 func TestUser_Validate(t *testing.T) {
-	u := model.TestUser(t)
-	assert.NoError(t, u.Validate())
+	testCase := []struct {
+		name    string
+		u       func() *model.User
+		isValid bool
+	}{
+		{
+			name: "validate",
+			u: func() *model.User {
+				return model.TestUser(t)
+			},
+			isValid: true,
+		},
+	}
+
+	for _, tc := range testCase {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.u().Validate())
+			} else {
+				assert.Error(t, tc.u().Validate())
+			}
+		})
+	}
+
 }
 
 func TestUser_BeforeCreate(t *testing.T) {
