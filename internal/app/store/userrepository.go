978276df1f5279500a4b85161d897ceecdda62b2
SharpDevOps10 <td090833@gmail.com>
2023-09-02 22:45:19 +0300

    Use `Validate` method in `Create`

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -9,6 +9,10 @@ type UserRepository struct {
 }
 
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+	if err := u.Validate(); err != nil {
+		return nil, err
+	}
+
 	if err := u.BeforeCreate(); err != nil {
 		return nil, err
 	}
