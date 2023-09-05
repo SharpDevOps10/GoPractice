cb292768e229e2e12ca7629794b6f8b4b619719c
Daniil Timofeev <71943754+SharpDevOps10@users.noreply.github.com>
2023-09-05 18:02:30 +0300

    refactoring #8: update configuration (#10)
    - added DatabaseURL
    - removed Store
    
    closes #8

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,17 +1,14 @@
 package apiserver
 
-import "github.com/SharpDevOps10/GoPractice/internal/app/store"
-
 type Config struct {
-	BindAddr string `toml:"bind_addr"`
-	LogLevel string `toml:"log_level"`
-	Store    *store.Config
+	BindAddr    string `toml:"bind_addr"`
+	LogLevel    string `toml:"log_level"`
+	DatabaseURL string `toml:"database_url"`
 }
 
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
 		LogLevel: "debug",
-		Store:    store.NewConfig(),
 	}
 }
