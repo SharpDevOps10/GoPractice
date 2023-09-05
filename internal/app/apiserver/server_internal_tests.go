5aba9eded039619face10cdcc1821c77e655858a
Daniil Timofeev <71943754+SharpDevOps10@users.noreply.github.com>
2023-09-05 18:43:06 +0300

    testing #13: created test for server (#14)
    - updated server.go
    - created HandleUsersCreate test
    
    closes #13

---
+++ internal/app/apiserver/server_internal_tests.go
@@ -0,0 +1,18 @@
+package apiserver
+
+import (
+	"github.com/SharpDevOps10/GoPractice/internal/app/store/teststore"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServer_HandleUsersCreate(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+	s := newServer(teststore.New())
+	s.ServeHTTP(rec, req)
+	assert.Equal(t, rec.Code, http.StatusOK)
+
+}
