94a2546f6f195e968ae4e2da08314c102d5f69b7
Daniil Timofeev <71943754+SharpDevOps10@users.noreply.github.com>
2023-09-12 23:23:40 +0300

    feature #2: crated user registration endpoint (#18)
    - updated User model
    - updated handleUsersCreate
    - added error and respond func
    
    closes #2

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	ID                int
-	Email             string
-	Password          string
-	EncryptedPassword string
+	ID                int    `json:"id"`
+	Email             string `json:"email"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"-"`
 }
 
 func (u *User) Validate() error {
@@ -41,4 +41,8 @@ func (u *User) BeforeCreate() error {
 	}
 
 	return nil
+}
+
+func (u *User) Sanitize() {
+	u.Password = ""
 }
