06b3a0dbea62f09b2d433c8617e6d70a2169f128
SharpDevOps10 <td090833@gmail.com>
2023-09-12 23:25:12 +0300

    testing #19: crated handleUsersCreate tests
    closes #19

--- internal/app/apiserver/server_internal_tests.go
+++ internal/app/apiserver/server_internal_tests.go
@@ -1,6 +1,8 @@
 package apiserver
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/SharpDevOps10/GoPractice/internal/app/store/teststore"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -9,10 +11,43 @@ import (
 )
 
 func TestServer_HandleUsersCreate(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
 	s := newServer(teststore.New())
-	s.ServeHTTP(rec, req)
-	assert.Equal(t, rec.Code, http.StatusOK)
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]interface{}{
+				"email":    "user@example.org",
+				"password": "secret",
+			},
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "invalid",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid params",
+			payload: map[string]interface{}{
+				"email":    "invalid",
+				"password": "short",
+			},
+			expectedCode: http.StatusUnprocessableEntity,
+		},
+	}
 
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			b := &bytes.Buffer{}
+			json.NewEncoder(b).Encode(tc.payload)
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodPost, "/users", b)
+			s.ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Code)
+		})
+	}
 }
