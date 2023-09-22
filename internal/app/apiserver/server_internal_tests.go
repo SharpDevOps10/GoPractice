a305a67a9da2074bf2430c95dbf1972874a32f4d
Daniil Timofeev <71943754+SharpDevOps10@users.noreply.github.com>
2023-09-22 19:41:09 +0300

    testing #29 created tests for auth endpoint (#30)
    closes #29

--- internal/app/apiserver/server_internal_tests.go
+++ internal/app/apiserver/server_internal_tests.go
@@ -3,6 +3,7 @@ package apiserver
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/SharpDevOps10/GoPractice/internal/app/model"
 	"github.com/SharpDevOps10/GoPractice/internal/app/store/teststore"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -46,6 +47,59 @@ func TestServer_HandleUsersCreate(t *testing.T) {
 			json.NewEncoder(b).Encode(tc.payload)
 			rec := httptest.NewRecorder()
 			req, _ := http.NewRequest(http.MethodPost, "/users", b)
+			s.ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Code)
+		})
+	}
+}
+
+func TestServer_HandleSessionsCreate(t *testing.T) {
+	store := teststore.New()
+	u := model.TestUser(t)
+	store.User().Create(u)
+	s := newServer(store)
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]interface{}{
+				"email":    u.Email,
+				"password": u.Password,
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "invalid",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid email",
+			payload: map[string]interface{}{
+				"email":    "bad email",
+				"password": u.Password,
+			},
+			expectedCode: http.StatusUnauthorized,
+		},
+		{
+			name: "invalid password",
+			payload: map[string]interface{}{
+				"email":    u.Email,
+				"password": "bad password",
+			},
+			expectedCode: http.StatusUnauthorized,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			b := &bytes.Buffer{}
+			json.NewEncoder(b).Encode(tc.payload)
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodPost, "/sessions", b)
 			s.ServeHTTP(rec, req)
 			assert.Equal(t, tc.expectedCode, rec.Code)
 		})
