d0a4a4a984d64f0a429defbf3a0b1501c04cb3ce
SharpDevOps10 <td090833@gmail.com>
2023-09-25 20:39:14 +0300

    feature #38 added FindById method
    - updated repository.go
    - added FindById
    
    closes #38

--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -4,5 +4,6 @@ import "github.com/SharpDevOps10/GoPractice/internal/app/model"
 
 type UserRepository interface {
 	Create(*model.User) error
+	FindById(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 }
--- internal/app/store/sqlstore/userrepository.go
+++ internal/app/store/sqlstore/userrepository.go
@@ -44,3 +44,22 @@ func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
 
 	return u, nil
 }
+
+func (r *UserRepository) FindById(id int) (*model.User, error) {
+	u := &model.User{}
+	if err := r.store.db.QueryRow(
+		"SELECT id, email, encrypted_password FROM userss WHERE id = $1",
+		id,
+	).Scan(
+		&u.ID,
+		&u.Email,
+		&u.EncryptedPassword,
+	); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, store.ErrRecordNotFound
+		}
+		return nil, err
+	}
+
+	return u, nil
+}
