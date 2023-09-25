3b69d17c45a43145449e440ae1ab2e791aa2e4a8
SharpDevOps10 <td090833@gmail.com>
2023-09-25 21:06:41 +0300

    testing #37 added tests for FindById:
    - fixed some tests
    - added FindById to userrepository.go
    - fixed store
    
    closes #37

--- internal/app/store/teststore/userrepository.go
+++ internal/app/store/teststore/userrepository.go
@@ -7,7 +7,7 @@ import (
 
 type UserRepository struct {
 	store *Store
-	users map[string]*model.User
+	users map[int]*model.User
 }
 
 func (r *UserRepository) Create(u *model.User) error {
@@ -19,14 +19,24 @@ func (r *UserRepository) Create(u *model.User) error {
 		return err
 	}
 
-	r.users[u.Email] = u
-	u.ID = len(r.users)
+	u.ID = len(r.users) + 1
+	r.users[u.ID] = u
 
 	return nil
 }
 
 func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
-	u, ok := r.users[email]
+	for _, u := range r.users {
+		if u.Email == email {
+			return u, nil
+		}
+	}
+
+	return nil, store.ErrRecordNotFound
+}
+
+func (r *UserRepository) FindById(id int) (*model.User, error) {
+	u, ok := r.users[id]
 	if !ok {
 		return nil, store.ErrRecordNotFound
 	}
